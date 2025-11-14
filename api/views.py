49a8d902f4b4a8cbfacd4e42a0d5ea4155457f6d
IbragimKhaniev <newinterny@gmail.com>
2025-11-14 17:54:56 +0300

    fix(api): remove drf-spectacular dependency from HelloView to prevent ImportError

--- api/views.py
+++ api/views.py
@@ -1,7 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.utils import timezone
-from drf_spectacular.utils import extend_schema
 from .serializers import MessageSerializer
 
 
@@ -10,9 +9,6 @@ class HelloView(APIView):
     A simple API endpoint that returns a greeting message.
     """
 
-    @extend_schema(
-        responses={200: MessageSerializer}, description="Get a hello world message"
-    )
     def get(self, request):
         data = {"message": "Hello!", "timestamp": timezone.now()}
         serializer = MessageSerializer(data)
